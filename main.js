ab807bf95702c9cfb6f89028f8e9313448eec37b
ashwin <ashwin300@gmail.com>
2020-06-05 14:00:40 -0500

    array1

---
+++ main.js
@@ -0,0 +1,14 @@
+const todos = [
+
+{id:1, text: "Item 1", isCompleted:false},
+
+{id:2,text:"Item 2",isCompleted:false,}
+
+
+
+
+]
+
+for(i in todos){
+    console.log(todos[i].text.toUpperCase())
+}
